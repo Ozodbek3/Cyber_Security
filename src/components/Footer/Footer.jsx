204d4e718ea2a75de89cc18ae19363bf76535f45
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2024-08-03 15:22:27 +0500

    footer completing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,20 +6,28 @@ const FooterDiv = styled.div`
     position: relative;
     bottom: 2%;
     width: 100%;
-    height: 50%;
+    height: 60%;
     background-color: ${({theme}) => theme.footer};
 `
 const Safetyh3 = styled.h3`
-  margin-top:16rem;
-  margin-left: 3rem;
+  margin-top:20rem;
+  margin-left: 40rem;
+`
+const FooterLine = styled.div`
+  position: relative;
+  left: 5rem;
+  top: 19rem;
+  width: 90%;
+  height: 1px;
+  background-color: white;
 `
 
 const Footer = () => {
   return (
     <FooterDiv>
-      <h3>Network security solutions</h3>
-      <Safetyh3>UzSafety Network </Safetyh3>
-      
+         <FooterLine></FooterLine>
+         <Safetyh3>Uzbekistan Safety Network</Safetyh3>
+         <p>©️ 2024 Uzbekistan Safety Network. All rights reserved.</p>
     </FooterDiv>
   );
 };
